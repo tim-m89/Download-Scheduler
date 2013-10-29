88787e581068e88e9c08f7457d5e8cfea9d45022
Tim Matthews <tim.matthews7@gmail.com>
2013-10-30 01:02:33 +1300

    Fixed a typo bug that prevented a default setting from working and therfore the addon until the setting was changed

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,5 +1,5 @@
 pref("dlScheduler.startTime", "2:00:00");
 pref("dlScheduler.finishTime", "8:00:00");
 pref("dlScheduler.dlScheduleList", "[]");
-pref("dlScheduler.pauseEnabled", true)
+pref("dlScheduler.pauseEnabled", true);
 
