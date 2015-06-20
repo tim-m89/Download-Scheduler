2bf2c33321ae81885d73bab47bd95f8f1909be4f
Tim Matthews <tim.matthews7@gmail.com>
2015-06-21 09:00:14 +1200

    Edit window changes

--- chrome/content/editWin.js
+++ chrome/content/editWin.js
@@ -1,92 +1,97 @@
-if(!tim_matthews) var tim_matthews={};
-if(!tim_matthews.downloadScheduler) tim_matthews.downloadScheduler={};
-if(!tim_matthews.downloadScheduler.editWin_js) tim_matthews.downloadScheduler.editWin_js = {};
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
 
-tim_matthews.downloadScheduler.editWin_js = {
+Cu.import("chrome://content/DownloadScheduer/DownloadScheduler.jsm");
 
-  slot: null,
+
+DownloadScheduler_editWin = {
+
+  getCurrentScheduleItem: function() {
+
+    if((window.arguments != null) && (window.arguments[0] != null))
+      return window.arguments[0];
+    else
+      return null;
+
+  },
 
   loadDownload: function() {
-    var index = window.arguments[0];
 
-    var urlBox = document.getElementById("tim_matthews.downloadScheduler.editWin.source");
-    var targetBox = document.getElementById("tim_matthews.downloadScheduler.editWin.target");
-    var timePicker1 = document.getElementById("tim_matthews.downloadScheduler.editWin.timepick");
-    var datePicker1 = document.getElementById("tim_matthews.downloadScheduler.editWin.datepick");
+    var urlBox      = document.getElementById("DownloadScheduler.editWin.source");
+    var targetBox   = document.getElementById("DownloadScheduler.editWin.target");
+    var timePicker  = document.getElementById("DownloadScheduler.editWin.timepick");
+    var datePicker  = document.getElementById("DownloadScheduler.editWin.datepick");
 
-    if(index>=0) {
-      var downloadArray = Application.storage.get("tim_matthews.downloadScheduler.downloadCtrl",  null).getDownloads();
-      this.slot = downloadArray[index];
+    var scheduleItem = DownloadScheduler_editWin.getCurrentScheduleItem();
 
-      urlBox.value = this.slot.source;
-      targetBox.value = this.slot.target;
+    if(scheduleItem != null) {
 
-      timePicker1.dateValue = new Date(this.slot.dateStart.getTime()); 
-      datePicker1.dateValue = new Date(this.slot.dateStart.getTime()); 
-    }
-    else {
-      var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-      if(prefs.prefHasUserValue("extensions.tim_matthews.dlScheduler.dlScheduleTime")) {
-        var now = new Date();
-        var lastStart = new Date(parseInt(prefs.getCharPref("extensions.tim_matthews.dlScheduler.dlScheduleTime"), 10));
-        var newStart = null;
+      urlBox.value    = scheduleItem.source;
+      targetBox.value = scheduleItem.target;
 
-        if(lastStart.getTime() > now.getTime()) {
-          newStart = lastStart;
-        } else {
-          newStart = new Date();
-          newStart.setHours( lastStart.getHours() );
-          newStart.setMinutes( lastStart.getMinutes() );
-          newStart.setSeconds( 0 );
-          
-          if(newStart.getTime() < now.getTime())
-            newStart.setTime( newStart.getTime() + 86400000 );
-        }
+      timePicker.dateValue = new Date(this.slot.dateStart.getTime());
+      datePicker.dateValue = new Date(this.slot.dateStart.getTime());
 
-        timePicker1.dateValue = new Date(newStart.getTime());
-        datePicker1.dateValue = new Date(newStart.getTime());
+    } else {
 
+      scheduleTime = DownloadScheduler.getDefaultScheduleItemTime();
+
+      if(scheduleTime != null) {
+        timePicker.dateValue = new Date(newStart.getTime());
+        datePicker.dateValue = new Date(newStart.getTime());
       }
 
-      urlBox.value = window.arguments[1];
-      targetBox.value = window.arguments[2];
+      if(window.arguments.size >= 2)
+        urlBox.value    = window.arguments[1];
+      if(window.arguments.size >= 3)
+        targetBox.value = window.arguments[2];
+
     }
+
 
   },
 
   chooseFile: function() {
-    var dlCtrl = Application.storage.get("tim_matthews.downloadScheduler.downloadCtrl",  null);
-    var src = document.getElementById("tim_matthews.downloadScheduler.editWin.source").value;
 
-    dlCtrl.urlChooseFile(src, function(fileName) {
+    var src = document.getElementById("DownloadScheduler.editWin.source").value;
+
+    DownloadScheduler.urlChooseFile(src, function(fileName) {
       if(fileName != null)
-        document.getElementById("tim_matthews.downloadScheduler.editWin.target").value = fileName;
+        document.getElementById("DownloadScheduler.editWin.target").value = fileName;
       window.focus();
-    });
+    } );
 
   },
 
   save: function() {
-    var index = window.arguments[0];
-    var downloadArray = Application.storage.get("tim_matthews.downloadScheduler.downloadCtrl",  null).getDownloads();
 
-    var newSlot = {};
-    newSlot.source = document.getElementById("tim_matthews.downloadScheduler.editWin.source").value;
-    newSlot.target = document.getElementById("tim_matthews.downloadScheduler.editWin.target").value;
-    newSlot.dateStart = document.getElementById("tim_matthews.downloadScheduler.editWin.datepick").dateValue;
 
-    var timeDate = document.getElementById("tim_matthews.downloadScheduler.editWin.timepick").dateValue;
-    newSlot.dateStart.setHours(timeDate.getHours());
-    newSlot.dateStart.setMinutes(timeDate.getMinutes());
-    newSlot.dateStart.setSeconds(0);
+    var source        = document.getElementById("DownloadScheduler.editWin.source").value;
+    var target        = document.getElementById("DownloadScheduler.editWin.target").value;
+    var startDate     = document.getElementById("DownloadScheduler.editWin.datepick").dateValue;
+    var startTime     = document.getElementById("DownloadScheduler.editWin.timepick").dateValue;
 
-    if(index>=0) {
-      Application.storage.get("tim_matthews.downloadScheduler.downloadCtrl",  null).updateSlot(this.slot, newSlot);
-    } else {
-      Application.storage.get("tim_matthews.downloadScheduler.downloadCtrl",  null).addOne(newSlot.source, newSlot.target, newSlot.dateStart);
+    var startDateTime = new Date( startDate.getTime() );
+      startDateTime.setHours( startTime.getHours() );
+      startDateTime.setMinutes( startTime.getMinutes() );
+      startDateTime.setSeconds(0);
+
+    var scheduleItem = DownloadScheduler_editWin.getCurrentScheduleItem();
+
+    if(scheduleItem != null) {
+      scheduleItem.source        = source;
+      scheduleItem.target        = target;
+      scheduleItem.startDateTime = startDateTime;
+      DownloadScheduler.itemUpdated( scheduleItem );
+    }
+    else {
+      scheduleItem = new DownloadScheduler.ScheduleItem( source, target, startDateTime );
+      DownloadScheduler.addItem( scheduleItem );
     }
 
     window.close();
+
   }
 
 };
