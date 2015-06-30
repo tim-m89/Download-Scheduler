5ff599ae184a8e70503b427dc450b6402a8ded00
Tim Matthews <tim.matthews7@gmail.com>
2015-06-30 22:13:00 +1200

    Re-init stop all timer on pref window unload

--- chrome/content/schedPref.js
+++ chrome/content/schedPref.js
@@ -2,6 +2,8 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 
+Cu.import("chrome://DownloadScheduler/content/DownloadScheduler.jsm");
+
 DownloadScheduler_schedPref = {
 
   prefLoad: function() {
@@ -9,14 +11,21 @@ DownloadScheduler_schedPref = {
       var timePickStop     = document.getElementById("DownloadScheduler.prefWin.timePickStop");
       var chkStopEnabled   = document.getElementById("DownloadScheduler.prefWin.chkStopEnabled");
 
-      var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+      var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
 
+      // this perference needs to be manuall loaded. Saving and loading of other prefs happens with the XUL alone.
       timePickStop.value      = prefs.getCharPref("extensions.DownloadScheduler.stopTime");
-      chkStopEnabled.checked  = prefs.getBoolPref("extensions.DownloadScheduler.stopEnabled");
 
       timePickStop.disabled = !chkStopEnabled.checked;
 
       chkStopEnabled.addEventListener("command", { handleEvent: function(ev){ timePickStop.disabled = !timePickStop.disabled; } }, false);
+
+  },
+
+  prefUnload: function() {
+
+    DownloadScheduler.shutdownStopAllTimer();
+    DownloadScheduler.initStopAllTimer();
 
   }
 
